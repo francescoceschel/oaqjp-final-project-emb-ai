60a74b869e1cc9758b72a25468a7be3c92f5c394
francescoceschel <francescoceschel@gmail.com>
2026-02-26 08:40:58 -0500

    project updatet up to the task 8, the final task

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+'''
+This function initiates the application of emotion detector
+'''
 from flask import Flask, request, render_template
 
 from EmotionDetection.emotion_detection import emotion_detector
@@ -6,6 +9,10 @@ app = Flask(__name__)
 
 @app.route('/emotionDetector')
 def emo_detector():
+    '''
+    This code receives a text and analyzes its emotional attributes, 
+    giving a rate and detecfting the dominant emotion
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyze)
 
@@ -17,7 +24,11 @@ def emo_detector():
     joy = result['joy']
     sadness = result['sadness']
     dominant_emotion = result['dominant_emotion']
-    return f'For the given statement, the system response is "anger": {anger}, "disgust": {disgust}, "fear": {fear}, "joy": {joy}, and "sadness": {sadness}. The dominant emotion is {dominant_emotion}'
+    return (
+        f"For the given statement, the system response is 'anger': {anger}, "
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, and "
+        f"'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
+    )
 
 @app.route("/")
 def render_index_page():
