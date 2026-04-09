32326be8ef8666a051d22e64665ea723bcd16bf3
Francesco <91471790+francescoceschel@users.noreply.github.com>
2026-04-09 10:06:22 +0200

    Update test cases for emotion detector messages

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -5,19 +5,19 @@ from EmotionDetection.emotion_detection import emotion_detector
 class TestEmotionDetector(unittest.TestCase):
 
     def test_emotion_detector(self):
-        result_1 = emotion_detector('I am glad this appened')
+        result_1 = emotion_detector('I am glad this occured')
         self.assertEqual(result_1['dominant_emotion'], 'joy')
 
-        result_2 = emotion_detector('I am really mad about this')
+        result_2 = emotion_detector('I am really angry about this')
         self.assertEqual(result_2['dominant_emotion'], 'anger')
 
-        result_3 = emotion_detector('I feel disgusted just hearing about this appened')
+        result_3 = emotion_detector('I feel disgusted just hearing about this')
         self.assertEqual(result_3['dominant_emotion'], 'disgust')
 
-        result_4 = emotion_detector('I am so sad about this')
+        result_4 = emotion_detector('I am so sad about what happened')
         self.assertEqual(result_4['dominant_emotion'], 'sadness')
 
-        result_5 = emotion_detector('I am really afraid that this will happen ')
+        result_5 = emotion_detector('I am really afraid about this')
         self.assertEqual(result_5['dominant_emotion'], 'fear')
 
 if __name__ == '__main__':
